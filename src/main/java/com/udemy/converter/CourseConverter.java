40c2b01c03c7955e51b660dfcf9fd4578b6342d4
alga <alejandrogp@gmail.com>
2018-06-12 17:08:48 +0200

    Transformar las entidades en modelos y viceversa

---
+++ src/main/java/com/udemy/converter/CourseConverter.java
@@ -0,0 +1,29 @@
+package com.udemy.converter;
+
+import com.udemy.entity.Course;
+import com.udemy.model.CourseModel;
+import org.springframework.stereotype.Component;
+
+@Component("courseConverter")
+public class CourseConverter {
+
+    //Entity -> Model
+    public CourseModel entityToModel(Course course){
+        CourseModel courseModel = new CourseModel();
+        courseModel.setName(course.getName());
+        courseModel.setDescripcion(course.getDescription());
+        courseModel.setPrice(course.getPrice());
+        courseModel.setHours(course.getHours());
+        return courseModel;
+    }
+
+    //Model -> Entity
+    public Course modelToEntity(CourseModel courseModel){
+        Course course = new Course();
+        course.setName(courseModel.getName());
+        course.setDescription(courseModel.getDescripcion());
+        course.setPrice(courseModel.getPrice());
+        course.setHours(courseModel.getHours());
+        return course;
+    }
+}
